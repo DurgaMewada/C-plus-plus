28262c13cd8b8eadae00627b10707fc75974e74b
DurgaMewada <mewadadurga83@gmail.com>
2024-01-31 23:49:53 +0530

    Update Employee.cpp

--- Class in c++/Employee.cpp
+++ Class in c++/Employee.cpp
@@ -1,4 +1,4 @@
-//	Q.1 Write a Program to create an Employee Record System using the Class and object of any 5 employees. Consider the below-mentioned attributes in the Employee class:
+//	Q. Write a Program to create an Employee Record System using the Class and object of any 5 employees. Consider the below-mentioned attributes in the Employee class:
 //	- emp_id
 //	- emp_name
 //	- emp_age
@@ -20,34 +20,61 @@ class Employee
 		
 		void input()
 		{
+			cin.ignore();
 			cout << "Enter Name:";
-			cin >> emp_name;
+		    getline(cin,emp_name);
+			
 			cout << "Enter Role :";
-			cin >> emp_role;
-			cout <<"Enter Role :";
-			cin >> emp_city;
-			cout << 
+			getline(cin,emp_role);
+			
+			cout <<"Enter City :";
+			getline(cin,emp_city);
+			
+			cout <<"Enter Company name  :";
+			getline(cin,emp_company_name);
+		
+			cout <<"Enter id of employee  :";
+			cin >> emp_id;
+			cout <<"Enter Age :";
+			cin >> emp_age;
+			cout <<"Enter Salary :";
+			cin >> emp_salary ;
+			cout <<"Enter Experience :";
+			cin >> emp_experience;
+			
 		}
 		
 		void output()
 		{
-			cout << emp_id << endl;
-			cout << emp_name << endl;
+			cout << "Name         :"<< emp_name << endl;
+			cout << "ID           :"<< emp_id << endl;
+			cout << "Age          :"<< emp_age << endl;
+			cout << "City         :"<< emp_city << endl;
+			cout << "Role         :"<< emp_role << endl;
+			cout << "Experience   :"<< emp_experience << endl;
+			cout << "Salary       :"<< emp_salary << endl;
+			cout << "Company name :"<< emp_company_name << endl;
 		}
 };
 int main()
 {
-	Employee e[5];
-	int i;
 	
-	for(i=0;i<5;i++)
+	int i,n;
+
+	cout << "Enter no. of Employee ";
+	cin >> n;
+	Employee e[n];
+	
+	for(i=0;i<n;i++)
 	{
-		cout<<endl;
+		cout << endl <<"      Employee " <<i+1;
+		cout << endl;
 		e[i].input();
 	}
 	
-	for(i=0;i<5;i++)
+	for(i=0;i<n;i++)
 	{
+		cout << endl <<"      Employee " <<i+1;
 		cout<<endl;
 		e[i].output();
 	}
