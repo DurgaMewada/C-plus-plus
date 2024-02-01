f3153b312e2398573e6d9c938cf50a2c3632e173
DurgaMewada <mewadadurga83@gmail.com>
2024-02-01 09:38:55 +0530

    Update Even element in array..cpp

--- Basic of c++/Even element in array..cpp
+++ Basic of c++/Even element in array..cpp
@@ -12,3 +12,38 @@
 //	Output:
 //	Even elements of an Array: 12, 42, 26
 
+#include<iostream>
+using namespace std;
+
+main()
+{
+	int n;
+	cout << "Enter size to create array :";
+	cin >> n;
+	
+	int a[n];
+	int i;
+	
+	for(i=0 ; i<n ; i++)
+	{
+		cout << "  :  ";
+		cin >>  a[i];
+	}
+	
+	cout << endl ;
+	
+	for(i=0 ; i<n ; i++)
+	{
+		cout << "Array element = " << a[i] << endl ;
+	}
+        
+    cout << endl << "Even element of an array are below : "<< endl;
+    
+	for(i=0 ; i<n ; i++)
+	{
+		if(a[i]%2==0)
+		{
+			cout << a[i] << "," ;
+		}
+	}
+}
