af1d6d07e39e887bb64fb3b53718ddf7055759de
DurgaMewada <mewadadurga83@gmail.com>
2024-02-01 09:55:48 +0530

    Update inch into feet..cpp

--- Basic of c++/inch into feet..cpp
+++ Basic of c++/inch into feet..cpp
@@ -5,3 +5,24 @@
 //	Enter inches: 40
 //	
 //	output: Feet - 9, Inch - 4
+
+#include<iostream>
+using namespace std;
+
+main()
+{
+	int feet,inch;
+	cout << "Enter feet : " ;
+	cin >> feet;
+	cout << "Enter inch : " ;
+	cin >> inch;
+	
+	int check;
+	
+	feet=feet+(inch/12);
+	inch=inch%12;
+	
+	cout << "\nAfter converting \n" ;
+	cout << feet << ":" << inch;
+
+}
