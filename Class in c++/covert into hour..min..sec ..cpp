2dcc6f7a7c6bee9a97177787db074149727b767c
DurgaMewada <mewadadurga83@gmail.com>
2024-02-01 10:03:12 +0530

    Update covert into hour..min..sec ..cpp

--- Class in c++/covert into hour..min..sec ..cpp
+++ Class in c++/covert into hour..min..sec ..cpp
@@ -1,1 +1,31 @@
 //	Q. Write a program to convert second into hh: mm: ss.
+#include<iostream>
+using namespace std;
+
+class Time
+{
+		int sec,min,hour;
+	public:
+	void convert()
+	{
+		
+	  cout << "Enter second for coverting them in hh/mm/ss format : ";
+	  cin >> sec;
+	  
+	   hour = sec/3600;
+	   sec=sec-(hour*3600);
+	   min=sec/60;
+	   sec=sec-(min*60);
+	   
+	   cout << endl << " Convert " <<  endl;
+	 cout <<"hh : mm : ss " << endl << hour << ':' << min << ':' << sec ; 
+	}
+   
+	
+	
+};
+main()
+{
+	Time t1;
+	t1.convert();
+}
